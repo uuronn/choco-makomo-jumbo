ac630ad3d73d0aaeaf49ca6eeec63dbdf357789c
tt1125 <tsurutsuru275@gmail.com>
2025-03-04 07:03:26 +0900

    AuthProviderを使用して認証機能を統合し、サインインおよびログアウトの処理を改善

--- front/src/app/[userId]/page.tsx
+++ front/src/app/[userId]/page.tsx
@@ -5,33 +5,33 @@ import { auth } from "~/lib/firebase";
 import { useRouter } from "next/navigation";
 
 export default function HomePage() {
-	const [user, setUser] = useState(auth.currentUser);
-	const router = useRouter();
+  const [user, setUser] = useState(auth.currentUser);
+  const router = useRouter();
 
-	useEffect(() => {
-		const unsubscribe = auth.onAuthStateChanged((user) => {
-			if (!user) {
-				router.push("/auth/signIn");
-			} else {
-				setUser(user);
-				router.push(`/${user.uid}`);
-			}
-		});
-		return () => unsubscribe();
-	}, [router]);
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (!user) {
+        router.push("/auth/signIn");
+      } else {
+        setUser(user);
+        router.push(`/${user.uid}`);
+      }
+    });
+    return () => unsubscribe();
+  }, [router]);
 
-	const handleLogout = async () => {
-		await auth.signOut();
-		router.push("/auth/signIn");
-	};
+  const handleLogout = async () => {
+    await auth.signOut();
+    router.push("/auth/signIn");
+  };
 
-	return (
-		<div className="flex flex-col items-center justify-center h-screen">
-			<h1 className="text-2xl font-bold">Dashboard</h1>
-			{user && <p className="mt-2">Welcome, {user.displayName}!</p>}
-			<button type="button" onClick={handleLogout} className="mt-4">
-				Logout
-			</button>
-		</div>
-	);
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+      {user && <p className="mt-2">Welcome, {user.displayName}!</p>}
+      <button type="button" onClick={handleLogout} className="mt-4">
+        Logout
+      </button>
+    </div>
+  );
 }
--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,36 +1,15 @@
 "use client";
 
-import { useEffect } from "react";
-import { auth } from "~/lib/firebase";
-import { useRouter } from "next/navigation";
+import { useAuth } from "./context/AuthProvider";
 
 export default function HomePage() {
-	// const [user] = useState(auth.currentUser);
-	const router = useRouter();
-
-	useEffect(() => {
-		const unsubscribe = auth.onAuthStateChanged((user) => {
-			if (!user) {
-				router.push("/auth/signIn");
-			} else {
-				router.push(`/${user.uid}`);
-			}
-		});
-		return () => unsubscribe();
-	}, [router]);
-
-	// const handleLogout = async () => {
-	// 	await auth.signOut();
-	// 	router.push("/auth/signIn");
-	// };
-
-	return (
-		<div className="flex flex-col items-center justify-center h-screen">
-			<h1 className="text-2xl font-bold">Dashboard</h1>
-			{/* {user && <p className="mt-2">Welcome, {user.displayName}!</p>}
-			<button type="button" onClick={handleLogout} className="mt-4">
-				Logout
-			</button> */}
-		</div>
-	);
+  const { handleSignOut } = useAuth();
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+      <button type="button" onClick={handleSignOut} className="mt-4">
+        Logout
+      </button>
+    </div>
+  );
 }
