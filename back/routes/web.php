64d405fad31f0a3aba5107ad3f4efe80683e6c6c
uuronn <uuronprogram@gmail.com>
2025-03-04 14:10:15 +0900

    test

--- back/routes/web.php
+++ back/routes/web.php
@@ -1,14 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 Route::get('/test', function () {
     return view('welcome');
 });
-
-Route::get('/users', [UserController::class, 'index']);
-
-// ユーザーを作成するPOSTルート
-Route::post('/users', [UserController::class, 'store'])->withoutMiddleware([VerifyCsrfToken::class])->middleware('cors');
