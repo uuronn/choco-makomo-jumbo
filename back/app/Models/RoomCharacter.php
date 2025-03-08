e96deee4a62269a0d4664a8b608ee8031110ac2e
uuronn <uuronprogram@gmail.com>
2025-03-08 19:15:53 +0900

    RoomCharacterモデル作成

---
+++ back/app/Models/RoomCharacter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RoomCharacter extends Model
+{
+    protected $table = 'room_characters';
+
+    protected $fillable = [
+        'room_id',
+        'character_id',
+        'level',
+        'life',
+        'power',
+        'speed',
+        'skill',
+    ];
+
+    public function room()
+    {
+        return $this->belongsTo(Room::class);
+    }
+}
