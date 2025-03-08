5ad27180615e351eb36b41396e7d06f3a3aa45e7
uuronn <uuronprogram@gmail.com>
2025-03-08 19:15:35 +0900

    Roomモデル作成

---
+++ back/app/Models/Room.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Room extends Model
+{
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = ['id', 'user_id', 'status'];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = (string) Str::uuid();
+            }
+        });
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function roomCharacters()
+    {
+        return $this->hasMany(RoomCharacter::class);
+    }
+}
