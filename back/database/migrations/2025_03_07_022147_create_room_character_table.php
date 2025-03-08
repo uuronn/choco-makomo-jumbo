5e9f3e45ca3c72ea719272ad9eefbe0b55e66f88
uuronn <uuronprogram@gmail.com>
2025-03-08 19:15:07 +0900

    ルームに必要なmigration作成

---
+++ back/database/migrations/2025_03_07_022147_create_room_character_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomCharacterTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('room_character', function (Blueprint $table) {
+            $table->id();
+            $table->string('room_id');
+            $table->string('character_id');
+            $table->integer('level');
+            $table->integer('life');
+            $table->integer('power');
+            $table->integer('speed');
+            $table->string('skill')->nullable();
+            $table->timestamps();
+
+            $table->foreign('room_id')->references('id')->on('rooms')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('room_character');
+    }
+}
