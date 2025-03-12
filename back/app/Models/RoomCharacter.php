60043aaa8b3af684f41f52f1e81014bb3985a161
uuronn <uuronprogram@gmail.com>
2025-03-12 17:26:36 +0900

    test

--- back/app/Models/RoomCharacter.php
+++ back/app/Models/RoomCharacter.php
@@ -3,12 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class RoomCharacter extends Model
 {
     protected $table = 'room_character';
 
+    protected $keyType = 'string'; // UUIDを文字列として扱う
+    public $incrementing = false;  // AUTO_INCREMENT を無効化
+
     protected $fillable = [
+        'id', // UUID を追加
         'room_id',
         'character_id',
         'level',
@@ -17,6 +22,16 @@ class RoomCharacter extends Model
         'speed',
         'skill',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = (string) Str::uuid(); // UUID を自動生成
+            }
+        });
+    }
 
     public function room()
     {
