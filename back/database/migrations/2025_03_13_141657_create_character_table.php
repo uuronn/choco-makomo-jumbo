252c123201bbba9802fd9ccd7913ca80831ac302
uuronn <uuronprogram@gmail.com>
2025-03-13 23:18:33 +0900

    テーブル追加

--- back/database/migrations/2025_03_13_141657_create_character_table.php
+++ back/database/migrations/2025_03_13_141657_create_character_table.php
@@ -6,20 +6,29 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('character', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('type');
+            $table->smallInteger('rarity');
+            $table->bigInteger('base_power');
+            $table->bigInteger('base_life');
+            $table->bigInteger('base_speed');
+            $table->uuid('active_skill_id')->nullable();
+            $table->uuid('passive_skill_id')->nullable();
+            $table->uuid('party_skill_id')->nullable();
+            $table->string('image_url');
             $table->timestamps();
+
+            // 外部キー制約
+            // $table->foreign('active_skill_id')->references('id')->on('skill')->onDelete('set null');
+            // $table->foreign('passive_skill_id')->references('id')->on('skill')->onDelete('set null');
+            // $table->foreign('party_skill_id')->references('id')->on('skill')->onDelete('set null');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('character');
