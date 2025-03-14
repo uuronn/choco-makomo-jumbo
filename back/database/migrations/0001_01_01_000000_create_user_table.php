989d32c0874b42b46fb9c620e20dfbb519b8a415
uuronn <uuronprogram@gmail.com>
2025-03-14 18:32:02 +0900

    defautlポイント

--- back/database/migrations/0001_01_01_000000_create_user_table.php
+++ back/database/migrations/0001_01_01_000000_create_user_table.php
@@ -9,12 +9,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('user', function (Blueprint $table) {
-            $table->string('id', 255)->primary(); // 文字列型のid（255文字）
+            $table->string('id', 255)->primary();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
-            $table->integer('point')->default(0); // pointを整数型に修正
+            $table->integer('point')->default(50);
             $table->rememberToken();
             $table->timestamps();
         });
@@ -27,7 +27,7 @@ return new class extends Migration
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->string('user_id', 255)->nullable()->index(); // user_idを文字列型に調整
+            $table->string('user_id', 255)->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
