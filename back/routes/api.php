92d1af879af19f62956bb3ec256f1561ca21138e
uuronn <uuronprogram@gmail.com>
2025-03-17 06:08:11 +0900

    ルート定義を整理し、ユーザーのキャラクター関連のエンドポイントを追加および修正。可読性を向上させるために引数名を変更。

--- back/routes/api.php
+++ back/routes/api.php
@@ -7,17 +7,6 @@ use App\Http\Controller\UserCharacterController;
 use App\Http\Controller\UserController;
 use Illuminate\Support\Facades\Route;
 
-
-
-
-
-
-Route::post('/users/{id}/gacha', [GachaController::class, 'gacha']);
-
-Route::get('/users/{id}/characters', [GachaController::class, 'characterList']);
-
-Route::put('/training', [UserCharacterController::class, 'levelUp']);
-
 Route::get('/rooms', [RoomController::class, 'index']);
 
 Route::post('/rooms', [RoomController::class, 'store']);
@@ -34,22 +23,32 @@ Route::post('/rooms/{roomId}', [RoomController::class, 'show']);
 
 Route::post('/rooms/simulate-battle', [RoomController::class, 'simulateBattle']);
 
-Route::delete('/users/{userId}/character', [UserCharacterController::class, 'destroy']);
-
 // ユーザーを作成する
 Route::post('/users', [UserController::class, 'create']);
 
 // すべてのユーザーを取得する
 Route::get('/users', [UserController::class, 'all']);
 
-// ユーザーが存在するか確認
+// ユーザーが存在するか確認する
 Route::get('/users/{userId}', [UserController::class, 'checkUser']);
 
-// ユーザーのpointを更新
+// ユーザーのpointを更新する
 Route::put('/users/{userId}/point', [UserController::class, 'updatePoint']);
+
+// ユーザーのキャラクター一覧を取得する
+Route::get('/users/{userId}/characters', [UserCharacterController::class, 'list']);
+
+// ユーザーのキャラクターを全て削除する
+Route::delete('/users/{userId}/characters', [UserCharacterController::class, 'delete']);
+
+// ユーザーのキャラクターをレベルアップする
+Route::put('/users/{userId}/characters/{characterId}', [UserCharacterController::class, 'levelUp']);
 
 // すべてのキャラクターを取得する
 Route::get('/characters', [CharacterController::class, 'all']);
 
 // 特定のキャラクターを取得する
 Route::get('/characters/{characterId}', [CharacterController::class, 'find']);
+
+// ガチャを引く
+Route::post('/gacha', [GachaController::class, 'gacha']);
