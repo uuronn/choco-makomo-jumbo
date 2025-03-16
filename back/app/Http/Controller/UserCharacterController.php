aa64b0dc7e987aafcbb1247b86e7ccb6a27b4b25
uuronn <uuronprogram@gmail.com>
2025-03-16 21:57:10 +0900

    user

--- back/app/Http/Controller/UserCharacterController.php
+++ back/app/Http/Controller/UserCharacterController.php
@@ -75,7 +75,7 @@ class UserCharacterController
         try {
             // バリデーション
             $request->validate([
-                'user_id' => 'required|string|exists:users,id',
+                'user_id' => 'required|string|exists:user,id',
                 'character_id' => 'required|string|exists:character,id',
                 'life' => 'required|integer|min:0', // 増加値
                 'power' => 'required|integer|min:0', // 増加値
