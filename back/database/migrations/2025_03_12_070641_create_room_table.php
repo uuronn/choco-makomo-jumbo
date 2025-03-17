9c8c40286456927da88eca3de724cf768e4f395e
uuronn <uuronprogram@gmail.com>
2025-03-18 03:12:18 +0900

    ルームテーブルにcurrentTurnUserIdとwinUserIdカラムを追加し、statusカラムの状態を更新

--- back/database/migrations/2025_03_12_070641_create_room_table.php
+++ back/database/migrations/2025_03_12_070641_create_room_table.php
@@ -6,27 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('room', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->uuid('hostUserId')->index();
             $table->uuid('guestUserId')->nullable()->index();
-            $table->string('status'); // ルームの状態（waiting, in_progress, finished など）
+            $table->string('currentTurnUserId')->nullable();
+            $table->string('winUserId')->nullable();
+            $table->string('status'); // ルームの状態（waiting, pending, battling, finished）
             $table->timestamps();
 
             // 外部キー制約（オプション）
             $table->foreign('hostUserId')->references('id')->on('user')->onDelete('cascade');
-            $table->foreign('guestUserId')->references('id')->on('user')->onDelete('set null'); // ゲストが抜けたらNULLにする
+            $table->foreign('guestUserId')->references('id')->on('user')->onDelete('set null');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('room');
