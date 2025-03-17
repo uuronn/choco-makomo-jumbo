eb4b52b34fa233aa52e869133a2693b88ecc500b
uuronn <uuronprogram@gmail.com>
2025-03-18 03:40:18 +0900

    RoomモデルにwinUserIdおよびcurrentTurnUserIdをfillableプロパティに追加

--- back/app/Model/Room.php
+++ back/app/Model/Room.php
@@ -14,7 +14,7 @@ class Room extends Model
 
     protected $hidden = ['created_at', 'updated_at'];
 
-    protected $fillable = ['id', 'hostUserId', 'guestUserId', 'status'];
+    protected $fillable = ['id', 'hostUserId', 'guestUserId', 'status', 'winUserId', 'currentTurnUserId'];
 
     protected static function boot()
     {
