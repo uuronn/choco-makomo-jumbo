3dfa6298d7a1f0d02f55a9007b3192ad4f830478
tt1125 <tsurutsuru275@gmail.com>
2025-03-17 12:21:03 +0900

    キャラクター一覧が表示されるように修正

--- front/src/app/characters/page.tsx
+++ front/src/app/characters/page.tsx
@@ -3,27 +3,7 @@
 import { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthProvider";
 import Image from "next/image";
-
-type Character = {
-  characterId: number;
-  level: number;
-  life: number;
-  power: number;
-  speed: number;
-
-  character: {
-    id: number;
-    name: string;
-    type: string;
-    image_url: string;
-    rarity: number;
-    base_life: number;
-    base_power: number;
-    base_speed: number;
-    skill: string;
-  };
-  userId: string;
-};
+import { Character } from "~/type/character";
 
 export default function CharacterListPage() {
   const { user } = useAuth();
@@ -34,13 +14,14 @@ export default function CharacterListPage() {
     if (user) {
       (async () => {
         const res = await fetch(
-          `${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${user.uid}/characters`,
+          `${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${user.uid}/characters`
         );
         if (!res.ok) {
           throw new Error("キャラクターの取得に失敗しました");
         }
 
         const data = await res.json();
+        console.log(data, "😄");
         setCharacterList(data);
       })();
     }
@@ -53,26 +34,24 @@ export default function CharacterListPage() {
       <h1 className="text-3xl font-bold mb-6">キャラ一覧</h1>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {characterList.map((userCharacter: Character) => (
+        {characterList.map((character: Character) => (
           <div
-            key={`${userCharacter.userId}-${userCharacter.characterId}`}
+            key={`${character.userId}-${character.characterId}`}
             className="bg-white p-4 rounded-lg shadow"
           >
-            <h2 className="text-xl font-semibold">
-              {userCharacter.character.name}
-            </h2>
+            <h2 className="text-xl font-semibold">{character.name}</h2>
             <Image
-              src={userCharacter.character.image_url}
+              src={character.image_url}
               alt="test"
               width={100}
               height={100}
             />
-            <p>タイプ：{userCharacter.character.type}</p>
-            <p>レア度: {userCharacter.character.rarity}</p>
-            <p>レベル: {userCharacter.level}</p>
-            <p>ライフ: {userCharacter.life}</p>
-            <p>パワー: {userCharacter.power}</p>
-            <p>スピード: {userCharacter.speed}</p>
+            <p>タイプ：{character.type}</p>
+            <p>レア度: {character.rarity}</p>
+            <p>レベル: {character.level}</p>
+            <p>ライフ: {character.life}</p>
+            <p>パワー: {character.power}</p>
+            <p>スピード: {character.speed}</p>
           </div>
         ))}
       </div>
