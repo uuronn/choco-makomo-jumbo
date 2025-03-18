ec03a5b97c72467e35f006d5c9060b87f804ff52
uuronn <uuronprogram@gmail.com>
2025-03-18 16:46:43 +0900

    ルームログ取得のAPIエンドポイントをPOSTからGETに変更し、RoomLogControllerを使用するように修正

--- back/routes/api.php
+++ back/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controller\CharacterController;
 use App\Http\Controller\GachaController;
 use App\Http\Controller\RoomController;
+use App\Http\Controller\roomLogController;
 use App\Http\Controller\UserCharacterController;
 use App\Http\Controller\UserController;
 use Illuminate\Support\Facades\Route;
@@ -84,7 +85,8 @@ Route::post('/{userId}/{roomId}/nextTurn', [RoomController::class, 'nextTurn']);
 
 
 // ルームログ取得
-Route::post('/rooms/{roomId}/log', [RoomController::class, 'logs']);
+Route::get('/rooms/{roomId}/log', [roomLogController::class, 'logs']);
+
 
 
 Route::put('/rooms/start-battle', [RoomController::class, 'startBattle']);
