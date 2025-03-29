d37d19fec23688d9fb18030336295e331c57fc05
uuronn <uuronprogram@gmail.com>
2025-03-30 08:58:58 +0900

    ユーザーキャラクターリストを取得するためのカスタムフックを追加

---
+++ front/src/hook/useUserCharacter.tsx
@@ -0,0 +1,35 @@
+import useSwr from "swr";
+
+export type Character = {
+	// userId: string;
+	characterId: string;
+	level: number;
+	life: number;
+	power: number;
+	speed: number;
+	name: string;
+	type: string;
+	baseEvasion: number;
+	specialSkillName: string;
+	specialSkillDescription: string;
+	specialSkillTurn: number;
+	passiveSkillName: string;
+	passiveSkillDescription: string;
+};
+
+const fetcher = async ([_, userId]: [string, string]): Promise<Character[]> => {
+	console.info("test", userId);
+	const res = await fetch(
+		`${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${userId}/characters`,
+	);
+
+	if (!res.ok) {
+		throw new Error("キャラクター一覧の取得に失敗しました");
+	}
+
+	return res.json();
+};
+
+export const useUserCharacterList = (userId: string | null) => {
+	return useSwr(userId ? ["characters", userId] : null, fetcher);
+};
