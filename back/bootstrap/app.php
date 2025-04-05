55396272b3462226164e87f0d98416b794236faf
uuronn <uuronprogram@gmail.com>
2025-04-05 18:46:01 +0900

    最後の活動を更新するミドルウェアをカーネルクラスに登録

--- back/bootstrap/app.php
+++ back/bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'firebase.auth' => \App\Http\Middleware\FirebaseAuth::class,
+            'update.last.activity' => \App\Http\Middleware\UpdateLastActivity::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
