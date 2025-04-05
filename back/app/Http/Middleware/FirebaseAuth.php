ab4cf676e2a25834d0aec0829b01b304289c3c33
uuronn <uuronprogram@gmail.com>
2025-04-05 19:02:48 +0900

    FirebaseAuthミドルウェアを修正し、ユーザーの存在を確認してからログインする機能を追加

--- back/app/Http/Middleware/FirebaseAuth.php
+++ back/app/Http/Middleware/FirebaseAuth.php
@@ -3,19 +3,17 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Kreait\Firebase\Contract\Auth;
-
+use App\Model\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth as LaravelAuth;
 class FirebaseAuth
 {
-    protected $auth;
+    public function __construct(protected Auth $auth) {}
 
-    public function __construct(Auth $auth)
-    {
-        $this->auth = $auth;
-    }
-
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
         $token = $request->bearerToken();
+
         if (!$token) {
             return response()->json(['message' => 'トークンが必要です'], 401);
         }
@@ -23,7 +21,17 @@ class FirebaseAuth
         try {
             $verifiedIdToken = $this->auth->verifyIdToken($token);
             $uid = $verifiedIdToken->claims()->get('sub');
+
+            $user = User::where('id', $uid)->first();
+
+            if (!$user) {
+                return response()->json(['message' => 'ユーザーが見つかりません'], 404);
+            }
+
+            LaravelAuth::login($user);
+
             $request->attributes->add(['firebase_uid' => $uid]);
+
             return $next($request);
         } catch (\Exception $e) {
             return response()->json(['message' => '認証エラー: ' . $e->getMessage()], 401);
