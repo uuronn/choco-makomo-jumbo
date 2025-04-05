0a8cacaef739f43c9900a8310f2896ac51d4c5d2
uuronn <uuronprogram@gmail.com>
2025-04-05 18:34:10 +0900

    ユーザーがEloquentモデルであることを確認してから最終活動時間を更新するようにミドルウェアを修正

--- back/app/Http/Middleware/UpdateLastActivity.php
+++ back/app/Http/Middleware/UpdateLastActivity.php
@@ -2,16 +2,22 @@
 
 namespace App\Http\Middleware;
 
+use App\Model\User;
 use Closure;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class UpdateLastActivity {
-    public function handle($request, Closure $next)
+class UpdateLastActivity
+{
+    public function handle(Request $request, Closure $next)
     {
-        if (Auth::auth()->check()) {
-            Auth::auth()->user()->update(['last_activity_at' => now()]);
+        if (Auth::check()) {
+            $user = Auth::user();
+            if ($user instanceof User) { // Eloquentモデルか確認
+                $user->update(['last_activity_at' => now()]);
+            }
         }
 
         return $next($request);
     }
-};
+}
