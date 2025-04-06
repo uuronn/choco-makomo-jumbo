50666772fe7a458891f5120aea13bcd7b914c9f5
uuronn <uuronprogram@gmail.com>
2025-04-06 18:04:20 +0900

    BugReportFormでreportTypeをtypeに変更するように修正

--- front/src/app/bugReportForm/page.tsx
+++ front/src/app/bugReportForm/page.tsx
@@ -123,7 +123,7 @@ export default function BugReportForm() {
 	};
 
 	const handleReportTypeChange = (value: string) => {
-		setFormData((prev) => ({ ...prev, reportType: value }));
+		setFormData((prev) => ({ ...prev, type: value }));
 	};
 
 	const validateForm = () => {
