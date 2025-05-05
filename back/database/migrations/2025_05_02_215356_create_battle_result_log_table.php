f53a4b724a476df2f359b3d26faafd40fb99383e
uuronn <uuronprogram@gmail.com>
2025-05-05 21:30:05 +0900

    マイグレーションクラスを無名クラスに変更

--- back/database/migrations/2025_05_02_215356_create_battle_result_log_table.php
+++ back/database/migrations/2025_05_02_215356_create_battle_result_log_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBattleResultLogsTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -23,4 +23,4 @@ class CreateBattleResultLogsTable extends Migration
     {
         Schema::dropIfExists('battle_result_logs');
     }
-}
+};
