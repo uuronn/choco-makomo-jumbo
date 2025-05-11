a0990c29a7f34e82ace996964908fd37d1416558
uuronn <uuronprogram@gmail.com>
2025-05-11 13:38:14 +0900

    User型を使用してPcHomePageおよびSpHomePageコンポーネントのプロパティ型をリファクタリング

--- front/src/app/home/components/pc/index.tsx
+++ front/src/app/home/components/pc/index.tsx
@@ -1,6 +1,10 @@
-export default function PcHomePage({
-	user,
-}: { user: { uid: string; name?: string } }) {
+import type { User } from "~/type/user";
+
+type Props = {
+	user: User;
+};
+
+export default function PcHomePage({ user }: Props) {
 	return (
 		<div className="p-10 text-left">
 			💻 デスクトップ版へようこそ！{user.name ?? user.uid} さん！
--- front/src/app/home/components/sp/index.tsx
+++ front/src/app/home/components/sp/index.tsx
@@ -1,6 +1,10 @@
-export default function SpHomePage({
-	user,
-}: { user: { uid: string; name?: string } }) {
+import type { User } from "~/type/user";
+
+type Props = {
+	user: User;
+};
+
+export default function SpHomePage({ user }: Props) {
 	return (
 		<div className="p-4 text-center">
 			📱 モバイル版です！こんにちは、{user.name ?? user.uid} さん！
