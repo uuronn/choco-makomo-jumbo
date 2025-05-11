e0affacb05cde1d2ca55ae0d96d0feffbb3347b3
uuronn <uuronprogram@gmail.com>
2025-05-11 13:37:53 +0900

    トークンからユーザー情報を取得する関数を新規作成

---
+++ front/src/lib/user.ts
@@ -0,0 +1,27 @@
+import type { User } from "~/type/user";
+import { adminAuth } from "./firebase-admin";
+
+export const fetchUserFromToken = async (token: string) => {
+	try {
+		const decoded = await adminAuth.verifyIdToken(token);
+
+		const res = await fetch(
+			`${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${decoded.uid}`,
+			{
+				headers: { Authorization: `Bearer ${token}` },
+			},
+		);
+
+		if (!res.ok) throw new Error("Failed to fetch user data");
+
+		const user = (await res.json()) as User;
+
+		if (!user) {
+			throw new Error("ユーザーが見つかりませんでした。");
+		}
+
+		return user;
+	} catch (e) {
+		console.error("⚠️ トークンの検証またはユーザー取得に失敗", e);
+	}
+};
