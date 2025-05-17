712eff4ba9f743c193d5d436f9d080bf916e5c13
uuronn <uuronprogram@gmail.com>
2025-05-17 13:27:46 +0900

    SectionContainerコンポーネントにクラス名のプロパティを追加し、スタイルの柔軟性を向上

--- front/src/components/SectionContainer.tsx
+++ front/src/components/SectionContainer.tsx
@@ -1,20 +1,31 @@
 import type { ComponentPropsWithoutRef, ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
 type Props = {
 	title: string;
 	icon: ReactNode;
 } & ComponentPropsWithoutRef<"section">;
 
-export const SectionContainer = ({ title, icon, children }: Props) => {
+export const SectionContainer = ({
+	title,
+	icon,
+	className,
+	children,
+}: Props) => {
 	return (
-		<section className="w-full bg-gray-900/80 border border-green-400/30 rounded-lg p-4">
+		<section
+			className={twMerge(
+				"w-full bg-gray-900/80 border border-green-400/30 rounded-lg p-4 overflow-hidden",
+				className,
+			)}
+		>
 			<h2 className="mb-4 text-xl font-bold text-green-400 flex gap-2 items-center">
 				{icon}
 				{title}
 				<div className="h-px flex-grow bg-gradient-to-r from-emerald-400 to-transparent" />
 			</h2>
 
-			<div className="overflow-y-auto h-[calc(100%-44px)]">{children}</div>
+			<div className="overflow-y-scroll h-[calc(100%-44px)]">{children}</div>
 		</section>
 	);
 };
