e515f09345e8184fead8130ce08ff55faa0df689
uuronn <uuronprogram@gmail.com>
2025-05-17 10:26:49 +0900

    BattleModeTabsコンポーネントのコメントアウトを追加

--- front/src/app/rooms/page.tsx
+++ front/src/app/rooms/page.tsx
@@ -17,8 +17,8 @@ export default async function RoomsPage() {
 	return (
 		<div className="min-h-screen bg-gray-900 flex flex-col items-center p-4">
 			<div className="w-full max-w-4xl">
-				<BattleModeTabs />
-				<div className="mt-8">
+				{/* <BattleModeTabs /> */}
+				<div>
 					<Suspense fallback={<div>読み込み中...</div>}>
 						<RoomsClient initialToken={token} />
 					</Suspense>
