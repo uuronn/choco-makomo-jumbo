68a12c96b2abf9b6db27b9d82991236bede445d2
uuronn <uuronprogram@gmail.com>
2025-06-07 22:37:13 +0900

    CharactersPageコンポーネントをリファクタリングし、MainContainerとSectionContainerを使用してレイアウトを整理

--- front/src/app/characters/page.tsx
+++ front/src/app/characters/page.tsx
@@ -3,6 +3,12 @@ import { Suspense } from "react";
 import { getTokenFromCookies } from "~/utils/token";
 import { fetchUserFromToken } from "~/lib/user";
 import { CharacterClient } from "./CharacterClient";
+import { TechPoint } from "~/components/TechPoint";
+import { CpuIcon, ZapIcon } from "lucide-react";
+import { MainContainer } from "~/components/MainContainer";
+import { SectionContainer } from "~/components/SectionContainer";
+import CharacterList from "./components/CharacterList";
+import { getCharaList } from "./charaList";
 
 export default async function CharactersPage() {
 	const cookieStore = await cookies();
@@ -13,9 +19,17 @@ export default async function CharactersPage() {
 		return <div className="p-4">ユーザー情報の取得に失敗しました</div>;
 	}
 
+	const userCharacterList = await getCharaList();
+
+	console.info("userCharacterList", userCharacterList);
+
 	return (
-		<Suspense fallback={<div>読み込み中...</div>}>
-			<CharacterClient initialToken={token} />
-		</Suspense>
+		<MainContainer title="技術育成" icon={<ZapIcon size={40} />}>
+			<TechPoint />
+
+			<SectionContainer title="所持技術" icon={<CpuIcon />} className="h-full">
+				<CharacterList userCharacterList={userCharacterList} />
+			</SectionContainer>
+		</MainContainer>
 	);
 }
