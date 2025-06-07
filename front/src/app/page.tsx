7119ee97d43b992ad80d71849e68fcaa567774fe
uuronn <uuronprogram@gmail.com>
2025-06-07 21:23:36 +0900

    IndexPageコンポーネントを修正し、ホームページへのリダイレクトを追加

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,7 +1,5 @@
-export default async function IndexPage() {
-	return (
-		<div className="flex flex-col items-center justify-center h-screen">
-			<h1 className="text-4xl font-bold">Welcome to the Index Page!</h1>
-		</div>
-	);
+import { redirect } from "next/navigation";
+
+export default function IndexPage() {
+	redirect("/home");
 }
