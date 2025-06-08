6a951d68666264d327aa1530eb52d955dcad0d91
uuronn <uuronprogram@gmail.com>
2025-06-08 12:37:10 +0900

    RootLayoutコンポーネントにmodalプロパティを追加し、子要素をClientProviders内に配置

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -14,8 +14,10 @@ const geistMono = Geist_Mono({
 
 export default function RootLayout({
 	children,
+	modal,
 }: Readonly<{
 	children: React.ReactNode;
+	modal: React.ReactNode;
 }>) {
 	return (
 		<html lang="ja">
@@ -25,7 +27,10 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				<ClientProviders>{children}</ClientProviders>
+				<ClientProviders>
+					{children}
+					{modal}
+				</ClientProviders>
 			</body>
 		</html>
 	);
