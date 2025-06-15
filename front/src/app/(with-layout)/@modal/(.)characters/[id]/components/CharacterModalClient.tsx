df15f6262cfb7489a1de6869fea6d253066a907f
uuronn <uuronprogram@gmail.com>
2025-06-15 13:55:53 +0900

    modal修正

--- front/src/app/(with-layout)/@modal/(.)characters/[id]/components/CharacterModalClient.tsx
+++ front/src/app/(with-layout)/@modal/(.)characters/[id]/components/CharacterModalClient.tsx
@@ -1,26 +1,12 @@
 "use client";
 
-import {
-	ActivityIcon,
-	Badge,
-	BicepsFlexedIcon,
-	CrosshairIcon,
-	FootprintsIcon,
-	GhostIcon,
-	HandshakeIcon,
-	HeartIcon,
-	Layers,
-	SparklesIcon,
-} from "lucide-react";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { CharacterImage } from "~/components/CharacterImage";
-import { CharacterStatus } from "~/components/CharacterStatus";
-import { CharacterStatusEditer } from "~/components/CharacterStatusEditer";
-import { Button } from "~/components/ui/button";
-import { characterToImagePath } from "~/lib/utils";
 import { CharacterAbilities } from "./CharacterAbilities";
+import { CharacterStatsEditor } from "./CharacterStatsEditor";
+import { XIcon } from "lucide-react";
+import type { CharacterType } from "~/type/character";
 
 export type Character = {
 	characterId: string;
@@ -41,38 +27,12 @@ export type Character = {
 	specialSkillTurn?: number;
 };
 
-type CharacterType =
-	| "バージョン管理"
-	| "データベース"
-	| "フレームワーク"
-	| "言語"
-	| "クラウド"
-	| "オペレーティングシステム"
-	| "実行環境"
-	| "ゲームエンジン"
-	| "コンテナー"
-	| "ライブラリ";
-
-const typeColors: Record<CharacterType, string> = {
-	バージョン管理: "bg-red-500",
-	データベース: "bg-blue-500",
-	フレームワーク: "bg-amber-700",
-	言語: "bg-green-500",
-	クラウド: "bg-yellow-400",
-	オペレーティングシステム: "bg-purple-800",
-	実行環境: "bg-pink-500",
-	ゲームエンジン: "bg-indigo-500",
-	コンテナー: "bg-teal-500",
-	ライブラリ: "bg-gray-500",
-};
-
 export function CharacterModalClient({
 	selectedCharacter,
 }: {
 	selectedCharacter: Character;
 }) {
 	const [statPoints, setStatPoints] = useState({ life: 0, power: 0, speed: 0 });
-	const [isAnimating, setIsAnimating] = useState(false);
 	const [isErrorState, setIsErrorState] = useState(false);
 	const canvasRef = useRef<HTMLCanvasElement>(null);
 
@@ -112,80 +72,36 @@ export function CharacterModalClient({
 				onClick={(e) => e.stopPropagation()}
 				className="bg-gray-900 text-white rounded-xl shadow-xl p-6 w-full max-w-5xl relative overflow-y-auto max-h-[90vh]"
 			>
-				<Button
+				<button
+					type="button"
 					className="absolute top-3 right-3"
 					onClick={() => router.back()}
 				>
-					閉じる
-				</Button>
+					<XIcon />
+				</button>
+
 				<div className="flex flex-col md:flex-row gap-4">
 					<div className="flex flex-col items-center">
 						<CharacterImage
-							selectedCharacter={selectedCharacter}
+							character={selectedCharacter}
 							isErrorState={isErrorState}
 							canvasRef={canvasRef}
 						/>
-						<h2 className="text-lg font-bold text-green-400">
+						<p className="text-lg font-bold text-green-400">
 							{selectedCharacter.name}【Lv.{selectedCharacter.level}】
-						</h2>
+						</p>
 					</div>
 
 					<CharacterAbilities character={selectedCharacter} />
 
-					<div className="flex-1 h-[290px] bg-gray-800/50 rounded-md border border-emerald-500/30 p-3">
-						<div className="space-y-2">
-							<CharacterStatusEditer
-								label="HP"
-								icon={<HeartIcon className="h-4 w-4 text-green-300 mr-1" />}
-								baseValue={selectedCharacter.life}
-								addedPoints={statPoints.life}
-								onDecrement={() => handleStatChange("life", false)}
-								onIncrement={() => handleStatChange("life", true)}
-								canDecrement={statPoints.life > 0}
-								canIncrement={remainingPoints > 0}
-							/>
-							<CharacterStatusEditer
-								label="パワー"
-								icon={
-									<BicepsFlexedIcon className="h-4 w-4 text-red-300 mr-1" />
-								}
-								baseValue={selectedCharacter.power}
-								addedPoints={statPoints.power}
-								onDecrement={() => handleStatChange("power", false)}
-								onIncrement={() => handleStatChange("power", true)}
-								canDecrement={statPoints.power > 0}
-								canIncrement={remainingPoints > 0}
-							/>
-							<CharacterStatusEditer
-								label="スピード"
-								icon={<FootprintsIcon className="h-4 w-4 text-blue-300 mr-1" />}
-								baseValue={selectedCharacter.speed}
-								addedPoints={statPoints.speed}
-								onDecrement={() => handleStatChange("speed", false)}
-								onIncrement={() => handleStatChange("speed", true)}
-								canDecrement={statPoints.speed > 0}
-								canIncrement={remainingPoints > 0}
-							/>
-							<CharacterStatus
-								label="回避率"
-								icon={<GhostIcon />}
-								value={selectedCharacter.baseEvasion}
-							/>
-							<CharacterStatus
-								label="クリティカル率"
-								icon={<CrosshairIcon />}
-								value={selectedCharacter.baseEvasion}
-							/>
-						</div>
-						<Button
-							className="mt-4 w-full bg-emerald-500 text-gray-900 hover:bg-green-400 font-bold"
-							style={{ boxShadow: "0 0 8px rgba(16, 185, 129, 0.5)" }}
-							onClick={handleDevelop}
-							disabled={usedPoints === 0}
-						>
-							育成する
-						</Button>
-					</div>
+					<CharacterStatsEditor
+						character={selectedCharacter}
+						handleStatChange={handleStatChange}
+						statPoints={statPoints}
+						remainingPoints={remainingPoints}
+						usedPoints={usedPoints}
+						handleDevelop={handleDevelop}
+					/>
 				</div>
 			</div>
 		</div>
