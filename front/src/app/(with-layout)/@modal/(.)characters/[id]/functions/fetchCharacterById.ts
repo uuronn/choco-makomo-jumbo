ecf49e2c6532d6c9fee0b68a34707ff42a596c37
uuronn <uuronprogram@gmail.com>
2025-06-28 19:25:26 +0900

    fetchCharacterById関数でキャッシュの再検証を60秒ごとに行う設定をコメントアウト

--- front/src/app/(with-layout)/@modal/(.)characters/[id]/functions/fetchCharacterById.ts
+++ front/src/app/(with-layout)/@modal/(.)characters/[id]/functions/fetchCharacterById.ts
@@ -15,6 +15,7 @@ export const fetchCharacterById = async (characterId: string) => {
 		`${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${user.id}/characters/${characterId}`,
 		{
 			headers: { Authorization: `Bearer ${token}` },
+			// next: { revalidate: 60 }, // キャッシュの再検証を60秒ごとに行う
 		},
 	);
 
