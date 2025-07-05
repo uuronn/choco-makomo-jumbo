525a9a0a59bff7b4385ede3a89402ea77cad1f5d
uuronn <uuronprogram@gmail.com>
2025-07-05 15:48:06 +0900

    RoomsPageコンポーネントからRoomsClientとCharacterListを削除し、SelectCharacterContainerを追加

--- front/src/app/(with-layout)/rooms/page.tsx
+++ front/src/app/(with-layout)/rooms/page.tsx
@@ -1,11 +1,10 @@
 import { cookies } from "next/headers";
 import { getTokenFromCookies } from "~/utils/token";
 import { fetchUserFromToken } from "~/lib/user";
-import { RoomsClient } from "./RoomsClient";
 import { MainContainer } from "~/components/MainContainer";
 import { SwordsIcon } from "lucide-react";
 import { SectionContainer } from "~/components/SectionContainer";
-import CharacterList from "../characters/components/CharacterList";
+import { SelectCharacterContainer } from "./_components/SelectCharacterContainer";
 
 export default async function RoomsPage() {
 	const cookieStore = await cookies();
@@ -18,11 +17,9 @@ export default async function RoomsPage() {
 
 	return (
 		<MainContainer title="対戦" icon={<SwordsIcon size={40} />}>
-			<RoomsClient initialToken={token}>
-				<SectionContainer title="所持技術" className="h-full">
-					<CharacterList />
-				</SectionContainer>
-			</RoomsClient>
+			<SelectCharacterContainer />
+
+			<SectionContainer title="ルーム一覧">test</SectionContainer>
 		</MainContainer>
 	);
 }
