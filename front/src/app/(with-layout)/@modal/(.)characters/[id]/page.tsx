ec5c1841face867fa0c3e7b4edfd2d984f8de98b
uuronn <uuronprogram@gmail.com>
2025-07-13 20:20:35 +0900

    feat: CharacterModalPageでユーザー情報の取得処理を追加し、エラーハンドリングを実装

--- front/src/app/(with-layout)/@modal/(.)characters/[id]/page.tsx
+++ front/src/app/(with-layout)/@modal/(.)characters/[id]/page.tsx
@@ -1,5 +1,8 @@
+import { cookies } from "next/headers";
 import { CharacterModal } from "./components/CharacterModal";
 import { fetchCharacterById } from "./functions/fetchCharacterById";
+import { getTokenFromCookies } from "~/utils/token";
+import { fetchUserFromToken } from "~/lib/user";
 
 export default async function CharacterModalPage({
 	params,
@@ -7,7 +10,17 @@ export default async function CharacterModalPage({
 	params: Promise<{ id: string }>;
 }) {
 	const { id } = await params;
-	const character = await fetchCharacterById(id);
 
-	return <CharacterModal character={character} />;
+	const character = await fetchCharacterById(id);
+	const cookieStore = await cookies();
+	const token = getTokenFromCookies(cookieStore);
+	const user = await fetchUserFromToken(token);
+
+	if (!user) {
+		return <div className="p-4">ユーザー情報の取得に失敗しました</div>;
+	}
+
+	return (
+		<CharacterModal character={character} userId={user.id} token={token} />
+	);
 }
