fd4910918f0a7764d8a69e1cd5f0a108d4be0d75
uuronn <uuronprogram@gmail.com>
2025-07-26 18:26:58 +0900

    feat: OtherPageコンポーネントを追加し、ユーザー情報の取得と表示を実装

---
+++ front/src/app/(with-layout)/ohter/page.tsx
@@ -0,0 +1,30 @@
+import { cookies } from "next/headers";
+import { getTokenFromCookies } from "~/utils/token";
+import { fetchUserFromToken } from "~/lib/user";
+import { MainContainer } from "~/components/MainContainer";
+import { Gamepad2Icon } from "lucide-react";
+// import { TechPoint } from "~/components/TechPoint";
+import { SectionContainer } from "~/components/SectionContainer";
+import FullscreenButton from "../home/components/FullscreenButton";
+// import { GameList } from "./components/GameList";
+
+export default async function OtherPage() {
+	const cookieStore = await cookies();
+	const token = getTokenFromCookies(cookieStore);
+	const user = await fetchUserFromToken(token);
+
+	if (!user) {
+		return <div className="p-4">ユーザー情報の取得に失敗しました</div>;
+	}
+
+	return (
+		<MainContainer title="その他" icon={<Gamepad2Icon size={40} />}>
+			{/* <TechPoint /> */}
+
+			<SectionContainer title="その他一覧" className="h-full">
+				<FullscreenButton />
+				{/* <GameList /> */}
+			</SectionContainer>
+		</MainContainer>
+	);
+}
