1ed9e1142933b6a5ac817e1a25c84f037ed60a25
uuronn <uuronprogram@gmail.com>
2025-07-26 20:04:45 +0900

    てｓｔ

--- front/src/app/(with-layout)/client-layout.tsx
+++ front/src/app/(with-layout)/client-layout.tsx
@@ -1,21 +1,30 @@
 "use client";
 
+import { useEffect, useRef } from "react";
 import { SnackbarProvider } from "notistack";
 import { FooterNavigation } from "~/components/FooterNavigation";
 import { MatrixRainCanvas } from "../_components/MatrixRainCanvas";
 
 export function ClientLayout({ children }: { children: React.ReactNode }) {
-	return (
-		<>
-			<SnackbarProvider
-				anchorOrigin={{ vertical: "top", horizontal: "right" }}
-				maxSnack={3}
-			>
-				<MatrixRainCanvas />
-				<div className="relative z-10">{children}</div>
+	const layoutRef = useRef<HTMLDivElement>(null);
 
-				<FooterNavigation />
-			</SnackbarProvider>
-		</>
+	useEffect(() => {
+		const width = layoutRef.current?.clientWidth;
+		console.log("Layout width:", width);
+	}, []);
+
+	return (
+		<SnackbarProvider
+			anchorOrigin={{ vertical: "top", horizontal: "right" }}
+			maxSnack={3}
+		>
+			<MatrixRainCanvas />
+			<div ref={layoutRef} className="relative z-10">
+				<p>{layoutRef.current?.clientWidth}</p>
+				<p>{layoutRef.current?.clientHeight}</p>
+				{children}
+			</div>
+			<FooterNavigation />
+		</SnackbarProvider>
 	);
 }
