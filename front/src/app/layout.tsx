ca96bbc459070ace2aaa0ef5fd58642c92a3ffbe
uuronn <uuronprogram@gmail.com>
2025-07-26 17:22:38 +0900

    feat: apple-mobile-web-app-capableメタタグを追加

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
 			<head>
 				<title>技術大戦争</title>
 				<meta name="mobile-web-app-capable" content="yes" />
+				<meta name="apple-mobile-web-app-capable" content="yes" />
 			</head>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
