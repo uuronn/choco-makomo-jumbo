bb7daed43b8affdac709135549fd89c6942da012
uuronn <uuronprogram@gmail.com>
2025-07-28 00:08:58 +0900

    fix: ReactNode削除

--- front/src/components/SectionContainer.tsx
+++ front/src/components/SectionContainer.tsx
@@ -1,4 +1,4 @@
-import type { ComponentPropsWithoutRef, ReactNode } from "react";
+import type { ComponentPropsWithoutRef } from "react";
 import { twMerge } from "tailwind-merge";
 
 type Props = {
