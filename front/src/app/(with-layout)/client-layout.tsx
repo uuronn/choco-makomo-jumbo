3f5329eb75d45a5f5283668cb761379a710289a4
uuronn <uuronprogram@gmail.com>
2025-07-27 14:35:34 +0900

    fix: useEffectを追加してwindow.innerHeightを表示するように修正

--- front/src/app/(with-layout)/client-layout.tsx
+++ front/src/app/(with-layout)/client-layout.tsx
@@ -1,71 +1,27 @@
 "use client";
 
-import { useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import { SnackbarProvider } from "notistack";
 import { FooterNavigation } from "~/components/FooterNavigation";
-// import { MatrixRainCanvas } from "../_components/MatrixRainCanvas";
 
 export function ClientLayout({ children }: { children: React.ReactNode }) {
+	const [innerHeight, setInnerHeight] = useState<number | null>(null);
 	const layoutRef = useRef<HTMLDivElement>(null);
 
-	// function useAppHeight() {
-	// 	console.info("window.innerHeight", window.innerHeight);
-	// 	useEffect(() => {
-	// 		const updateAppHeight = () => {
-	// 			document.documentElement.style.setProperty(
-	// 				"--app-height",
-	// 				`${window.innerHeight}px`,
-	// 			);
-	// 		};
-
-	// 		updateAppHeight();
-	// 		window.addEventListener("resize", updateAppHeight);
-	// 		return () => window.removeEventListener("resize", updateAppHeight);
-	// 	}, []);
-	// }
-
-	// useAppHeight();
-
-	// const [dimensions, setDimensions] = useState<{
-	// 	width: number;
-	// 	height: number;
-	// }>({ width: 0, height: 0 });
-
-	// useEffect(() => {
-	// 	if (!layoutRef.current) return;
-
-	// 	const resize = () => {
-	// 		setDimensions({
-	// 			width: layoutRef.current?.clientWidth || 0,
-	// 			height: layoutRef.current?.clientHeight || 0,
-	// 		});
-	// 	};
-
-	// 	resize(); // 初期取得
-	// 	window.addEventListener("resize", resize); // リサイズ時にも反映
-	// 	return () => window.removeEventListener("resize", resize);
-	// }, []);
+	useEffect(() => {
+		setInnerHeight(window.innerHeight);
+	}, []);
 
 	return (
-		// <div ref={layoutRef} className="bg-red-200 min-h-screen">
-		// 	{/* SP版でこれが出現する */}
-		// 	<div className="h-[40px] bg-red-600">test</div>
-		// 	<div className="bg-red-200 p-4">
-		// 		<p>Width: {dimensions.width}px</p>
-		// 		<p>Height: {dimensions.height}px</p>
-		// 	</div>
 		<SnackbarProvider
 			anchorOrigin={{ vertical: "top", horizontal: "right" }}
 			maxSnack={3}
 		>
-			<p>{window.innerHeight}</p>
-			<div className="flex flex-col h-screen">
-				{/* <MatrixRainCanvas /> */}
-				{/* <div className="relative z-10">{children}</div> */}
+			<p>{innerHeight !== null ? `${innerHeight}px` : "読み込み中..."}</p>
+			<div ref={layoutRef} className="flex flex-col h-screen">
 				<div className="h-full flex justify-center">{children}</div>
 				<FooterNavigation />
 			</div>
 		</SnackbarProvider>
-		// </div>
 	);
 }
