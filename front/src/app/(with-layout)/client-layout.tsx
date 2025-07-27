d0efa1e18a7457e4e02cc3caaf0c3160a1c8c400
uuronn <uuronprogram@gmail.com>
2025-07-27 15:34:03 +0900

    fix: ClientLayoutコンポーネントの子要素のパディングを削除

--- front/src/app/(with-layout)/client-layout.tsx
+++ front/src/app/(with-layout)/client-layout.tsx
@@ -47,7 +47,7 @@ export function ClientLayout({ children }: { children: React.ReactNode }) {
 			{/* Using ref to potentially manipulate layout later if needed */}
 			{/* <div ref={layoutRef} className="flex flex-col"> */}
 			<div ref={layoutRef} className="flex-col flex justify-center">
-				<div className="m-auto w-full h-full px-4 py-3">{children}</div>
+				<div className="m-auto w-full h-full">{children}</div>
 
 				<FooterNavigation />
 			</div>
