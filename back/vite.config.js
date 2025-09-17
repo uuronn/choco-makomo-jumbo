d148d01a5529a40c532e4c3df2c8cfab89c33212
uuronn <uuronprogram@gmail.com>
2025-09-18 00:09:12 +0900

    fix: '@vitejs/plugin-react-swc'を追加し、Vite設定を更新

--- back/vite.config.js
+++ back/vite.config.js
@@ -3,10 +3,11 @@ import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
-
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
 	plugins: [
+        react(),
 		laravel({
 			input: [
 				"resources/css/app.css",
