d1347ce470f93d422c40923a2cea1b0d3e59cb8c
uuronn <uuronprogram@gmail.com>
2025-09-17 23:38:23 +0900

    fix: Appコンポーネントを削除し、RouterProviderを使用するように変更

--- back/resources/ts/app.tsx
+++ back/resources/ts/app.tsx
@@ -2,31 +2,44 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { create } from "zustand";
 import "../css/app.css"; // Tailwind
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
 
 const useStore = create<{ count: number; inc: () => void }>((set) => ({
 	count: 0,
 	inc: () => set((s) => ({ count: s.count + 1 })),
 }));
 
-function App() {
-	const { count, inc } = useStore();
-	return (
-		<div className="flex flex-col items-center justify-center h-screen">
-			<h1 className="text-2xl font-bold">Hello Laravel + React + TS</h1>
-			<p className="mt-4">Count: {count}</p>
-			<button
-				type="button"
-				className="mt-2 px-4 py-2 bg-blue-500 text-white rounded"
-				onClick={inc}
-			>
-				Increment
-			</button>
-		</div>
-	);
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
 }
+
+
+// function App() {
+// 	const { count, inc } = useStore();
+// 	return (
+// 		<div className="flex flex-col items-center justify-center h-screen">
+// 			<h1 className="text-2xl font-bold">Hello Laravel + React + TS</h1>
+// 			<p className="mt-4">Count: {count}</p>
+// 			<button
+// 				type="button"
+// 				className="mt-2 px-4 py-2 bg-blue-500 text-white rounded"
+// 				onClick={inc}
+// 			>
+// 				Increment
+// 			</button>
+// 		</div>
+// 	);
+// }
 
 ReactDOM.createRoot(document.getElementById("app") as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+	<RouterProvider router={router} />
 	</React.StrictMode>,
 );
