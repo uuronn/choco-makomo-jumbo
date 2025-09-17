b0296ac869da9ff3deb1163da4630d2747d1d9d6
uuronn <uuronprogram@gmail.com>
2025-09-17 23:42:21 +0900

    fix: ルート設定を変更し、'/'から'{any}'にマッピングしてReactアプリを表示

--- back/routes/web.php
+++ back/routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', function () {
+    return view('app'); // React を埋め込んだ Blade
+})->where('any', '.*');
