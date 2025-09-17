95369b10bc6355f205d0939e7065441d0381f16e
uuronn <uuronprogram@gmail.com>
2025-09-17 22:34:10 +0900

    fix: インデントを修正し、PWAのマニフェストリンクを整形

--- back/resources/views/welcome.blade.php
+++ back/resources/views/welcome.blade.php
@@ -9,8 +9,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-  <link rel="manifest" href="/build/manifest.webmanifest">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/build/manifest.webmanifest">
 {{--
         <!-- Styles / Scripts -->
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
