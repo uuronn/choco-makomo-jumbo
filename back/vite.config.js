8b72028577f93104696f55ecb9a08ab178048a45
uuronn <uuronprogram@gmail.com>
2025-09-17 21:52:36 +0900

    Refactor CSS and PWA setup
    - Deleted old Tailwind CSS file and replaced it with a new one.
    - Updated manifest.json to reflect the new CSS file.
    - Added a new web manifest file for PWA configuration.
    - Implemented service worker registration script.
    - Created a new service worker file for caching strategies.
    - Added Workbox library for enhanced service worker capabilities.
    - Updated Vite configuration to include PWA plugin.

--- back/vite.config.js
+++ back/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
 	plugins: [
